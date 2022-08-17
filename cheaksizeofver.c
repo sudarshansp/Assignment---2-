ffab863e8464f830a75677265d8d0a32d97b9680
sudarshansp <111242373+sudarshansp@users.noreply.github.com>
2022-08-17 20:06:52 +0530

    Create cheaksizeofver.c

---
+++ cheaksizeofver.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+  int a = sizeof(a);
+  printf("size of int a is: %d byte",a);
+  printf("\n");
+  float b = sizeof(b);
+  printf("size of float b is: %f byte",b);
+  printf("\n");
+  char c = sizeof(c);
+  printf("size of char c is: %d byte",c);
+  printf("\n");
+  double d = sizeof(d);
+  printf("size of double d is: %f byte",d);
+}

70b8f8705b798dccbb0524d1523dc526150968fb
sudarshansp <111242373+sudarshansp@users.noreply.github.com>
2022-08-18 19:18:20 +0530

    Create apndnumdigi.c

---
+++ apndnumdigi.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+  int a,b;
+  printf("Enter a Number: ");
+  scanf("%d",&a);
+  printf("Enter a digit: ");
+  scanf("%d",&b);
+  printf("Ans is: %d%d",a,b);
+  return 0;
+}

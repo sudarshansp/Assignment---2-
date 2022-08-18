48bcac55c07b8a27f1b3234c276c95620b1f3817
sudarshansp <111242373+sudarshansp@users.noreply.github.com>
2022-08-18 19:11:15 +0530

    Create usdtoinr.c

---
+++ usdtoinr.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+  int n;
+  float div;
+  printf("enter indain rupies: ");
+  scanf("%d",&n);
+  div = n / 76.23;
+  printf("USD price is: %f",div);
+  return 0;
+}
